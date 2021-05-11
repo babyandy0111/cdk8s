02e7638a551faf73c82161ff2654800eff6cc5b4
Faryne <faryne@gmail.com>
2021-05-11 18:01:45 +0800

    proceeding traefik

--- main.ts
+++ main.ts
@@ -1,12 +1,16 @@
 import { Construct } from 'constructs';
-import { App, Chart, ChartProps } from 'cdk8s';
+import {ApiObject, App, Chart, ChartProps} from 'cdk8s';
+import {TraefikService, TraefikDeployment} from "./props/traefik"
 
 export class MyChart extends Chart {
   constructor(scope: Construct, id: string, props: ChartProps = { }) {
     super(scope, id, props);
 
     // define resources here
-
+    // -- 建立 Traefik Service / Deployment
+    let obj1 = new ApiObject(this, "traefik-service", TraefikService)
+    let obj2 = new ApiObject(this, "traefik-deployment", TraefikDeployment)
+    obj2.addDependency(obj1)
   }
 }
 
---
+++ props/traefik.ts
@@ -0,0 +1,13 @@
+let TraefikDeployment = {
+    apiVersion: "v1",
+    kind: "Foo"
+};
+let TraefikService = {
+    apiVersion: "v1",
+    kind: "Foo"
+};
+
+export {
+    TraefikService,
+    TraefikDeployment
+}
